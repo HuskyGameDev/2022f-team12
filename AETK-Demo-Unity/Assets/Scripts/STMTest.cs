fd555787ca07d2ddfa8f21c0dc10652e30a849fd
Gabe <goetjens@gmail.com>
2022-09-26 20:30:19 -0400

    Testing Dialogue + Quads

--- AETK-Demo-Unity/Assets/Scripts/STMTest.cs
+++ AETK-Demo-Unity/Assets/Scripts/STMTest.cs
@@ -23,11 +23,29 @@ public class STMTest : MonoBehaviour
             switch (textNum)
             {
                 case 0:
-                    stm.text = "<drawAnim=Fill><c=green>I'm feeling a <s=0.5>little</s>...\n<drawAnim=Stamp><j=dayquil>sleepy</j>";
+                    stm.text = "<f=8-bit><drawAnim=Stamp><j>ASSMAN, ASSMAN!!!<d=4>\nPLEASE COME QUICK!";
                     break;
                 case 1:
-                    stm.text = "<readDelay=0.05><drawAnim=Stamp><c=green>Once upon a time,\nIn the great great greens of <u>Herocity</u>";
+                    stm.text = "<f=8-bit><drawAnim=Stamp>Ok, so:";
                     break;
+                case 2:
+                    stm.text = "<f=8-bit><drawAnim=Stamp><w=loop>There was this crazy guy chasing me down the street!";
+                    break;
+                case 3:
+                    stm.text = "<f=8-bit><drawAnim=Stamp>He was <sub>lanky</sub>,<d=8> <w=seasick>strange</w>,<d=8> <sup>oblong,</sup><d=8> <w>obtuse...</w>";
+                    break;
+                case 4:
+                    stm.text = "<f=8-bit><drawAnim=Stamp><w=loop>Roundtuse</w>,<d=6> quirky,<d=6> goofy...<d=15>\nand his face looked like this --><q=pog>";
+                    break;
+                /*case 0:
+                    stm.text = "<f=8-bit><drawAnim=Fill><c=green>I'm feeling a <s=0.5>little</s>...\n<drawAnim=Stamp><j=dayquil>sleepy</j>";
+                    break;
+                case 1:
+                    stm.text = "<f=8-bit><drawAnim=Stamp><c=green>Once upon a time,\nIn the great great greens of Herocity";
+                    break;
+                case 2:
+                    stm.text = "<f=8-bit><drawAnim=Stamp><c=green>I'm feeling <s=0.5>(a little)</s> <j=dayquil>sleepy</j> look --> <q=clap>";
+                    break;*/
                 default:
                     stm.text = "from da top";
                     textNum = -1;
